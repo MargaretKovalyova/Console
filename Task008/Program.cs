dc058260a6fca58474d29398a8b0e9f68924aa5c
margarita <margarikovalyova@yandex.by>
2023-09-26 21:05:08 +0300

    change programm 8

--- Task008/Program.cs
+++ Task008/Program.cs
@@ -10,14 +10,6 @@ int number = Convert.ToInt32(Console.ReadLine());
 int count = 1;
 
 while (count <= number)
-{
     if (count % 2 == 0)
-    {
-        Console.Write($"{count} ");
-        count = count + 1;
-    }   
-    else
-    {
-        count = count + 1;
-    } 
-}
+    Console.Write($"{count} ");
+count = count + 1;
