b1644ca845199a336d06fe201fd3ee7f5235a57f
margarita <margarikovalyova@yandex.by>
2023-09-26 22:36:10 +0300

    added task012

---
+++ Task012/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая выводит третью цифру 
+// pаданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+
+
+
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100)
+{
+    Console.WriteLine("Третьего числа нет");
+}
+   
+while (number > 999)
+{
+    number/= 10;
+}
+Console.WriteLine($"{number} -> {number % 10}");
+
+
