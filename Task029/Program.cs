36fba2fb7fc81bc75b8a586d7b09b6983a906ab9
margarita <margarikovalyova@yandex.by>
2023-10-10 22:11:49 +0300

    create task029

--- Task029/Program.cs
+++ Task029/Program.cs
@@ -25,3 +25,27 @@ void PrintArray(int[] array)
 
 int[] arr = CreateArray(8);
 PrintArray(arr);
+
+// int[] CreateArrayRndInt(int size, int min, int max)
+// {
+// int[] arr = new int[size];
+// Random rnd = new Random();
+
+// for (int i = 0; i < size; i++)
+// {
+// arr[i] = rnd.Next(min, max + 1);
+// }
+
+// return arr;
+// }
+
+// void PrintArray(int[] arr)
+// {
+// Console.Write("[");
+// for (int i = 0; i < arr.Length; i++)
+// {
+// if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+// else Console.Write($"{arr[i]}");
+// }
+// Console.WriteLine("]");
+// }
